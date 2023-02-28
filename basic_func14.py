047fccffbce25d77760499de3fd9ab52a81b0791
sherbek342 <sherbekrustamov342@gmail.com>
2023-02-28 14:55:50 +0400

    14

--- basic_func14.py
+++ basic_func14.py
@@ -1,3 +1,4 @@
+import math
 def main(a, b):
     '''find the floor division of a and b and return it.
     
@@ -8,4 +9,7 @@ def main(a, b):
     Returns:
         int: the result.
     '''
-    return 
+    javob=math.floor(a/b)
+    return javob
+
+print(main(11, 2))
